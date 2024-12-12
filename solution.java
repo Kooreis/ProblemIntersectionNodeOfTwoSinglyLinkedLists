817381b8267a2416601462a2ee32c9a6338a3447
Kooreis <kooreis.official@gmail.com>
2024-12-12 12:14:58 -0600

    This part of the script is responsible for taking user input to create the first linked list. It prompts the user to enter the number of nodes and the elements of the linked list.

--- solution.java
+++ solution.java
@@ -1,12 +1,15 @@
-import java.util.HashSet;
-import java.util.Scanner;
-
-class Node {
-    int data;
-    Node next;
-
-    Node(int d) {
-        data = d;
-        next = null;
-    }
-}
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Node head1 = null, head2 = null;
+        System.out.println("Enter the number of nodes in the first linked list:");
+        int n1 = scanner.nextInt();
+        System.out.println("Enter the elements of the first linked list:");
+        if(n1 > 0) {
+            head1 = new Node(scanner.nextInt());
+            Node temp = head1;
+            for(int i = 1; i < n1; i++) {
+                temp.next = new Node(scanner.nextInt());
+                temp = temp.next;
+            }
+        }
