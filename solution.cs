467686c6fdb95cc261fabdb4ec027f0c8ff33ae2
Kooreis <kooreis.official@gmail.com>
2024-12-12 12:14:58 -0600

    This part of the script creates two linked lists that intersect at node2. It then calls the GetIntersection method to find the intersection node and prints the value of the intersection node if it exists.

--- solution.cs
+++ solution.cs
@@ -1,8 +1,29 @@
-using System;
-using System.Collections.Generic;
-
-public class Node
+public class Program
 {
-    public int Value { get; set; }
-    public Node Next { get; set; }
-}
+    static void Main(string[] args)
+    {
+        Node node1 = new Node { Value = 1 };
+        Node node2 = new Node { Value = 2 };
+        Node node3 = new Node { Value = 3 };
+        Node node4 = new Node { Value = 4 };
+        Node node5 = new Node { Value = 5 };
+
+        Node list1 = node1;
+        node1.Next = node2;
+        node2.Next = node3;
+
+        Node list2 = node4;
+        node4.Next = node5;
+        node5.Next = node2;
+
+        Node intersection = GetIntersection(list1, list2);
+
+        if (intersection != null)
+        {
+            Console.WriteLine("The intersection node is: " + intersection.Value);
+        }
+        else
+        {
+            Console.WriteLine("There is no intersection node.");
+        }
+    }
